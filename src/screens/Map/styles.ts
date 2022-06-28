0738148db021b18352c891ef65f9ec3d09ec4bb3
Irys <iryssalvatico16@gmail.com>
2022-06-28 12:36:14 -0300

    fim

--- src/screens/Map/styles.ts
+++ src/screens/Map/styles.ts
@@ -1,4 +1,5 @@
 import { StyleSheet, Dimensions } from "react-native";
+import colors from "../../styles/colors";
 
 const styles = StyleSheet.create({
     container: {
@@ -15,6 +16,34 @@ const styles = StyleSheet.create({
         width: Dimensions.get("window").width,
         height: Dimensions.get("window").height,
     },
+    posicao:{
+        position: "absolute",
+        top: 0,
+        left: 0,
+        zIndex: 0,
+        width: "100%",
+    },
+    google: {
+        width: Dimensions.get("window").width,
+    },
+    rowsearch:{
+        flexDirection: "row",
+        justifyContent:"space-between",
+        backgroundColor: colors.green,
+        borderRadius: 10,
+        margin: 10,
+        alignItems: "center",
+        width:"99%",
+        height: 65,
+    },
+    input: {
+        width: "70%",
+    },
+    icon:{
+        fontSize: 24,
+        padding: 5,
+    },
 })
+
 
 export default styles
